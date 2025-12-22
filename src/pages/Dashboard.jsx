60dbdfbc8526cfa49c2500d2ff359b88e0574b0d
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2025-12-22 23:54:23 +0600

    write the functionality to clear the UI after manullay clear the local storage add a event on window

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react";
 import Coffee from "../components/Coffee";
 import Heading from "../components/Heading";
 import { getTheFavorite } from "../utils/FavoriteServer";
 
 const Dashboard = () => {
-  const storedCoffees = getTheFavorite();
+  const [coffees, setCoffees] = useState(() => getTheFavorite() || []);
+
+  useEffect(() => {
+    const handleStorageEvent = () => {
+      setCoffees(() => getTheFavorite() || []);
+    };
+
+    window.addEventListener("storage", handleStorageEvent);
+
+    return () => {
+      window.removeEventListener("storage", handleStorageEvent);
+    };
+  }, []);
 
   return (
     <div>
@@ -17,7 +30,7 @@ const Dashboard = () => {
 
       {/* dynamic cards:: add  favorite coffees */}
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 place-items-center mt-10">
-        {storedCoffees.map((coffee) => (
+        {coffees.map((coffee) => (
           <Coffee key={coffee?.id} coffee={coffee}></Coffee>
         ))}
       </div>
